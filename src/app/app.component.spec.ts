5d1a089a98002ed068b765829c4e29d379afc69b
Kevin Phelps <kevinphelps@users.noreply.github.com>
2019-05-02 11:42:03 -0500

    test: clean up app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,28 @@
-import { async, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let fixture: ComponentFixture<AppComponent>;
+  let nativeElement: HTMLElement;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent]
-    }).compileComponents();
-  }));
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    nativeElement = fixture.nativeElement;
   });
 
-  it(`should have as title 'ng-starter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-starter');
+  it('should create the app', () => {
+    expect(fixture.componentInstance).toBeTruthy();
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-starter!');
+    expect(nativeElement.querySelector('h1').textContent).toContain('Welcome to ng-starter!');
   });
 });
